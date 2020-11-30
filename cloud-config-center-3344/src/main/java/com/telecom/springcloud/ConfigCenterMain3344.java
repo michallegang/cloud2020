c6b13fa767510099da37abec641627ece7e5e8eb
zhangxiaogang <17805128748@163.com>
2020-11-30 16:06:19 +0800

    springcloud-config服务端代码提交

---
+++ cloud-config-center-3344/src/main/java/com/telecom/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.telecom.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Date：2020-11-30 15:16
+ * Description：
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
