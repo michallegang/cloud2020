5e8ccce66729d7219ded18c3fd75e3c2bc1ca3a5
zhangxiaogang <17805128748@163.com>
2020-12-04 10:10:09 +0800

    SpringCloudAlibaba的提供者9001,9002

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/telecom/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.telecom.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Date：2020-12-03 19:39
+ * Description：
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/telecom/springcloud/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.telecom.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Date：2020-12-04 9:56
+ * Description：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
