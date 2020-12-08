53e23fab1dbbf6f639b2e83a3f9f54249cda0671
zhangxiaogang <17805128748@163.com>
2020-12-08 11:24:32 +0800

    springcloud-config客户端代码提交

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/telecom/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.telecom.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Date：2020-12-04 11:19
+ * Description：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/telecom/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.telecom.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Date：2020-12-04 11:21
+ * Description：
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
